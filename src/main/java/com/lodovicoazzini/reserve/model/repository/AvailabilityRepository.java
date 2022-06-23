9db744a28edc30ca9b4cdac9f8e018bc1395a91b
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-06-24 00:39:44 +0200

    remove old backend

--- src/main/java/com/lodovicoazzini/reserve/model/repository/AvailabilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lodovicoazzini.reserve.model.repository;
-
-import com.lodovicoazzini.reserve.model.entity.Availability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
-}
