71cab25583463ef4ef435210801a7a8b7ab47462
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-06-28 11:41:14 +0200

    update reservations page

--- re-serve-frontend/src/components/ReserveDialog.vue
+++ re-serve-frontend/src/components/ReserveDialog.vue
@@ -1,46 +1,44 @@
 <template>
-    <v-row justify="center">
-        <v-dialog v-model="dialog" persistent max-width="600px">
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" block dark v-bind="attrs" v-on="on">
-                    reserve slot
+    <v-dialog v-model="dialog" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" block dark v-bind="attrs" v-on="on">
+                reserve slot
+            </v-btn>
+        </template>
+        <v-card>
+            <v-card-title>
+                <span class="text-h5">Other User Email</span>
+            </v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                label="Email*"
+                                required
+                                v-model="email"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error" text @click="dialog = false">
+                    close
                 </v-btn>
-            </template>
-            <v-card>
-                <v-card-title>
-                    <span class="text-h5">Other User Email</span>
-                </v-card-title>
-                <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col cols="12">
-                                <v-text-field
-                                    label="Email*"
-                                    required
-                                    v-model="email"
-                                ></v-text-field>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                    <small>*indicates required field</small>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="error" text @click="dialog = false">
-                        close
-                    </v-btn>
-                    <v-btn
-                        color="primary"
-                        text
-                        @click="handleNext"
-                        :disabled="email == ''"
-                    >
-                        next
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </v-row>
+                <v-btn
+                    color="primary"
+                    text
+                    @click="handleNext"
+                    :disabled="email == ''"
+                >
+                    next
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
--- re-serve-frontend/src/views/ReserveSlotView.vue
+++ re-serve-frontend/src/views/ReserveSlotView.vue
@@ -13,9 +13,65 @@
 
 <script>
 import MyCalendar from '@/components/calendar/MyCalendar.vue';
+
 export default {
-    components: { MyCalendar },
+    components: {
+        MyCalendar,
+    },
+    data() {
+        return {
+            reserveDialog: false,
+            availabilities: [],
+        };
+    },
+    provide() {
+        return {
+            getEvents: () => this.availabilities,
+            events: this.availabilities,
+            saveEvent: this.saveEvent,
+            deleteEvent: this.deleteEvent,
+        };
+    },
+    mounted() {
+        this.reloadAvailabilities();
+    },
     methods: {
+        saveEvent(event) {
+            this.backendLink.get(
+                `availability/create/${event.start}/${event.end}`,
+                () => this.reloadAvailabilities(),
+                (message) => {
+                    console.log(message);
+                    this.reloadAvailabilities();
+                }
+            );
+        },
+        deleteEvent(event) {
+            this.backendLink.get(
+                `availability/remove/${event.start}/${event.end}`,
+                () => this.reloadAvailabilities(),
+                (message) => {
+                    console.log(message);
+                    this.reloadAvailabilities();
+                }
+            );
+        },
+        reloadAvailabilities() {
+            this.backendLink.get(
+                `availability/list`,
+                (response) => {
+                    const mapped = response.data.map((availability) => ({
+                        start: availability.startTime,
+                        end: availability.endTime,
+                        name: null,
+                        color: 'green',
+                        timed: true,
+                    }));
+                    this.availabilities = mapped;
+                },
+                (message) => console.log(message)
+            );
+        },
         backCallback() {
             this.$router.push({ name: 'my-availabilities' }).catch(() => {});
         },
